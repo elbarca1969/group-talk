9a2178d6c5d409c119f3f0d0c60a8ca830cf7438
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-11-23 11:49:08 +0900

    groupsテーブルの作成

--- db/migrate/20201123024716_create_groups.rb
+++ db/migrate/20201123024716_create_groups.rb
@@ -1,7 +1,8 @@
 class CreateGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :groups do |t|
-
+      t.string :name, null: false, unique: true
+      t.string :content
       t.timestamps
     end
   end

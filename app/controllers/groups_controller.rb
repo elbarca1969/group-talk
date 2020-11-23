f3384ae0a7b42907fda1b729cb1715817e54fdf1
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-11-23 12:09:51 +0900

    new, createアクションの記述

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,5 +2,23 @@ class GroupsController < ApplicationController
   before_action :authenticate_user!, only: :new
 
   def new
+    @group = Group.new
+    @group.users << current_user
   end
+
+  def create
+    @group = current_user.groups.new(group_params)
+    if current_user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  
+  def group_params
+    params.require(:group).permit(:name, :content, user_ids: [])
+  end
+
 end

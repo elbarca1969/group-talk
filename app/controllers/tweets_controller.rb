8100eddaa0c8f756420dbf7ea079fc6cd9dceae5
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-11-24 12:26:58 +0900

    グループのページへ遷移

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,8 @@
 class TweetsController < ApplicationController
+  
   def index
+    @group = Group.find(params[:group_id])
+    @tweets = @group.tweets.includes(:user).order("created_at DESC")
   end
+
 end

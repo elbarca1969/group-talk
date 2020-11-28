ec9f318bb96a65ae83c8b04dd01dbfc942e816c7
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-11-28 17:52:36 +0900

    set_groupを定義し、before_actionで記述

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_group, only: [:show, :edit, :update, :destroy, :join, :quit]
 
   def index
   end
@@ -20,19 +21,16 @@ class GroupsController < ApplicationController
   end
 
   def show
-    @group = Group.find(params[:id])
     @tweets = @group.tweets.includes(:user).order("created_at DESC")
   end
 
   def edit
-    @group = Group.find(params[:id])
     unless user_signed_in? && current_user.id == @group.user_id
       redirect_to root_path
     end
   end
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to group_path(@group.id)
     else
@@ -41,7 +39,6 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
     @group.destroy
     redirect_to list_groups_path
   end
@@ -51,7 +48,6 @@ class GroupsController < ApplicationController
   end
 
   def join
-    @group = Group.find_by(id: params[:id])
     if !@group.users.include?(current_user)
       @group.users << current_user
       redirect_to group_tweets_path(@group)
@@ -59,7 +55,6 @@ class GroupsController < ApplicationController
   end
 
   def quit
-    @group = Group.find(params[:id])
     @group.users.delete(current_user)
     redirect_to root_path
   end
@@ -72,6 +67,10 @@ class GroupsController < ApplicationController
   
   def group_params
     params.require(:group).permit(:name, :content, user_ids: [])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end

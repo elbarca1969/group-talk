e18e3400ef6d5fb4d00f68613b1a4c3dce459c25
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-11-28 14:38:09 +0900

    groupsコントローラーにupdateアクションの記述

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -23,6 +23,15 @@ class GroupsController < ApplicationController
     @group = Group.find(params[:id])
   end
 
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to group_path(@group.id)
+    else
+      render :edit
+    end
+  end
+
   def show
     @group = Group.find(params[:id])
     @tweets = @group.tweets.includes(:user).order("created_at DESC")

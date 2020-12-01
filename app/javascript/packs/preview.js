ec0fc99fcae6242c87c1c42b9c33912d58aca4c0
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-12-01 15:00:56 +0900

    input要素で値の変化が起こった時に呼び出される関数を定義

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
   const ImageList = document.getElementById('image-list');
+
+  document.getElementById('tweet_image').addEventListener('change', function(e){
+    console.log(123);
+  });
 });

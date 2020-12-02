caa58f36a2d26afbb4351fdfa4771cfc656b42fe
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-12-02 17:09:51 +0900

    tweetsテーブルの編集

--- db/migrate/20201124031424_create_tweets.rb
+++ db/migrate/20201124031424_create_tweets.rb
@@ -1,7 +1,7 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-      t.string :content, null: false
+      t.string :content
       t.references :group, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps

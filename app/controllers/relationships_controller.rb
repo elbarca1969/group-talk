0e8d9ab7601c4fea29960cd287aa37595a31d457
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-12-04 18:37:19 +0900

    コントローラーにcreate, destroyアクションを記述

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,22 @@
 class RelationshipsController < ApplicationController
+  before_action :set_user, only: [:create, :destroy]
+
+  def create
+    following = current_user.follow(@user)
+    following.save
+    redirect_to @user
+  end
+
+  def destroy
+    following = current_user.unfollow(@user)
+    following.destroy
+    redirect_to @user
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:follow_id])
+  end
+
 end

915fc285217614b4fd723e3868031e145b075827
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-12-04 18:48:57 +0900

    コントローラーにfollowing, followerアクションを記述

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -13,6 +13,16 @@ class RelationshipsController < ApplicationController
     redirect_to @user
   end
 
+  def following
+    @user = User.find(params[:id])
+    @followings = @user.followings 
+  end
+
+  def follower
+    @user = User.find(params[:id])
+    @followers = @user.followers
+  end
+
   private
 
   def set_user

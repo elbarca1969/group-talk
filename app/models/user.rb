f33f375898bf8cdaada1e8d7f1a149c462e6eda2
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-12-04 18:28:13 +0900

    RelationshipモデルとUserモデルにアソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,10 @@ class User < ApplicationRecord
   has_many :groups, through: :group_users
   has_many :tweets
   has_many :owned_groups, class_name: "Group"
+
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
 end

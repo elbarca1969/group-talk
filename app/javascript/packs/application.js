490c150d1fce681334ecdb1ac3cc89c8f9601a2c
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-12-19 11:11:36 +0900

    ログアウト時にエラーにならないようにJSの記述を変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@fortawesome/fontawesome-free/js/all").start()
+import '@fortawesome/fontawesome-free/js/all';
 require("@rails/ujs").start()
 //require("turbolinks").start()
 require("@rails/activestorage").start()

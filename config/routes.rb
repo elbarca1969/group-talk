348025ea1ea7aed02db949e2a20dec969894c4fb
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2020-12-28 13:57:20 +0900

    ルーティングのusersにlistアクションを記述

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,11 @@ Rails.application.routes.draw do
     end
     resources :tweets
   end
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    member do
+      get :list
+    end
+  end
   resources :relationships, only: [:index, :create, :destroy] do
     member do
       get :following

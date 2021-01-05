f82f7b50f3d29fb594a496b21244bb96edce2653
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-01-05 16:52:51 +0900

    全角1文字を半角2文字カウント、それ以外を半角1文字カウントにバリデーションを設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,7 +3,8 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :content, presence: true, unless: :was_attached?
+  validates :content, presence: true, unless: :was_attached?,
+                      length_with_wide_char: { maximum: 400 }
 
   def was_attached?
     self.image.attached?

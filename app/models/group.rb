68c907698756befff2095161ceb2d550e1127b9a
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-01-06 14:22:33 +0900

    グループの内容の最大文字数を半角200文字に設定

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,6 +6,7 @@ class Group < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true,
                   length_with_wide_char: { maximum: 70 }
+  validates :content, length_with_wide_char: { maximum: 200 }
 
   def self.search(search)
     if search != ""

d9479f642e23264234b47a39afe320dfaf9c05bd
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-01-07 14:26:45 +0900

    groupモデルの単体テストコードを日本語対応に合わせて変更

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -13,19 +13,25 @@ RSpec.describe Group, type: :model do
     it "nameが空では登録できないこと" do
       @group.name = ""
       @group.valid?
-      expect(@group.errors.full_messages).to include("Name can't be blank")
+      expect(@group.errors.full_messages).to include("Nameを入力してください")
     end
-
+    
     it "重複したnameが存在する場合登録できないこと" do
       @group.save
       another_group = FactoryBot.build(:group, name: @group.name)
       another_group.valid?
-      expect(another_group.errors.full_messages).to include("Name has already been taken")
+      expect(another_group.errors.full_messages).to include("Nameはすでに存在します")
     end
   
     it "contentが空でも登録できること" do
       @group.content = ""
-      expect(@group). to be_valid
+      expect(@group).to be_valid
+    end
+
+    it "userが紐付いていないと保存できないこと" do
+      @group.user = nil
+      @group.valid?
+      expect(@group.errors.full_messages).to include("Userを入力してください")
     end
   end
 end

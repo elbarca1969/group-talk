c6615f685ddb4dbb86e8256415c8b28bd5b75a53
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-01-11 14:00:57 +0900

    usersコントローラーのストロングパラメーターに:avatorを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :introduction, :email)
+    params.require(:user).permit(:nickname, :introduction, :email, :avator)
   end
 
   def set_user

c225be7d7f58b2a46353967ce44a769f2c88c5b7
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-01-12 18:39:20 +0900

    「グループを削除」ボタン以外の場所をクリックしたら確認モーダルが閉じる

--- app/javascript/packs/groupDelete.js
+++ app/javascript/packs/groupDelete.js
@@ -2,4 +2,10 @@ $(function(){
   $(document).on('click', '#group-delete-open', function(){
     $('#group-delete-modal').show();
   });
+  $(document).on('click', '#group-delete-btn-cancel', function(){
+    $('#group-delete-modal').hide();
+  });
+  $(document).on('click', '#group-delete-modal', function(){
+    $('#group-delete-modal').hide();
+  });
 });

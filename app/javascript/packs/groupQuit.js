975a54e7d504a25eec96d7c02538fbf087d458e6
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-01-13 17:33:03 +0900

    「退会する」ボタン以外の場所をクリックしたら確認モーダルが閉じる

--- app/javascript/packs/groupQuit.js
+++ app/javascript/packs/groupQuit.js
@@ -2,4 +2,10 @@ $(function(){
   $(document).on('click', '#group-quit-open', function(){
     $('#group-quit-modal').show();
   });
+  $(document).on('click', '#group-quit-btn-cancel', function(){
+    $('#group-quit-modal').hide();
+  });
+  $(document).on('click', '#group-quit-modal', function(){
+    $('#group-quit-modal').hide();
+  });
 });

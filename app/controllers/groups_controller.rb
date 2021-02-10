e79a042c4a12db455c2de16060dec723bdf04632
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-10 14:20:30 +0900

    グループ詳細ページのN+1問題の解消

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,7 +22,7 @@ class GroupsController < ApplicationController
 
   def show
     @groups = Group.select("groups.*, COUNT(group_users.id) users_count").left_joins(:group_users).group("groups.id").order("users_count desc").limit(20)
-    @tweets = @group.tweets.includes(:user).order("created_at DESC")
+    @tweets = @group.tweets.with_attached_image.order("created_at DESC").includes(:user, :likes, :image_attachment, user: { avator_attachment: :blob })
   end
 
   def edit

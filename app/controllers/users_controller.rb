51eda56136d1587ec5cafd6d7d8a0afec0998fad
elbarca1969 <72918547+elbarca1969@users.noreply.github.com>
2021-02-10 15:01:29 +0900

    usersコントローラーにright_barメソッドを定義し記述をまとめる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: :edit
   before_action :set_user, only: [:show, :edit, :update, :list, :owner, :purge]
+  before_action :right_bar, only: [:show, :list, :owner]
 
   def show
-    @groups = Group.select("groups.*, COUNT(group_users.id) users_count").left_joins(:group_users).group("groups.id").order("users_count desc").limit(20)
     @tweets = @user.tweets.with_attached_image.order("created_at DESC").includes(:group, :likes, :image_attachment)
   end
 
@@ -22,12 +22,10 @@ class UsersController < ApplicationController
   end
 
   def list
-    @groups = Group.select("groups.*, COUNT(group_users.id) users_count").left_joins(:group_users).group("groups.id").order("users_count desc").limit(20)
     @groupLists = @user.groups.select("groups.*, COUNT(group_users.id) users_count").left_joins(:group_users).group("groups.id").order("users_count desc").includes(:group_users, :users, :user)
   end
 
   def owner
-    @groups = Group.select("groups.*, COUNT(group_users.id) users_count").left_joins(:group_users).group("groups.id").order("users_count desc").limit(20)
     @groupOwners = @user.owned_groups.select("groups.*, COUNT(group_users.id) users_count").left_joins(:group_users).group("groups.id").order("users_count desc").includes(:group_users, :users)
   end
 
@@ -44,6 +42,10 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def right_bar
+    @groups = Group.select("groups.*, COUNT(group_users.id) users_count").left_joins(:group_users).group("groups.id").order("users_count desc").limit(20)
   end
 
 end
